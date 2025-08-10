cb2018621edd5dd64255b35e6404b6b7cbd42a19
VENU GOPAL RAMIREDDY <venugopalrvg9@gmail.com>
2025-08-10 15:59:41 +0530

    Add files via upload

---
+++ Day 3 CRT.java
@@ -0,0 +1,150 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+/*pyramid*/
+/*
+class Main {
+    public static void main(String[] args) {
+        int n=5,j,i;
+        for(i=1;i<=n;i++){
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+*/
+/*
+
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+
+*/
+
+/* Reverse pyramid*/
+/*
+class Main {
+    public static void main(String[] args) {
+        int n=5,j,i;
+        for(i=n;i>=1;i--){
+            for(int k=1;k<=n-i;k++){
+                System.out.print("-");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+*/
+/*
+
+ * * * * * 
+- * * * * 
+-- * * * 
+--- * * 
+---- * 
+
+*/
+
+/* Diamond*/
+/*class Main {
+    public static void main(String[] args) {
+        int n=5,j,i;
+        for(i=1;i<=n;i++){
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+         for(i=n-1;i>=1;i--){
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+    }
+}*/
+
+
+/*
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+     
+*/
+
+/* */
+/*
+class Main {
+    public static void main(String[] args) {
+        int n=5,j,i;
+        for(i=1;i<=n;i++){
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+/*
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+/*
+class Main {
+    public static void main(String[] args) {
+        int n=5,j,i;
+        for(i=1;i<=n;i++){
+            for(int k=1;k<=n-i;k++){
+                System.out.print("");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
+
+
